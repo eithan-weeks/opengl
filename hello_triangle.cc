3f2190ccc0dd16aa5685e4d9341baa70399f9d1c
Desmond Rhodes <desmond.rhodes@outlook.com>
2023-05-11 19:44:05 +0000

    Hello Triangle

---
+++ hello_triangle.cc
@@ -0,0 +1,157 @@
+#include <iostream>
+#include <glad/glad.h>
+#include <GLFW/glfw3.h>
+
+int hello_triangle() {
+	glfwInit();
+	glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
+	glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
+	glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
+
+	GLFWwindow* window {glfwCreateWindow(1440, 1080, "Hello Triangle", nullptr, nullptr)};
+	if (!window) {
+		glfwTerminate();
+		return -1;
+	}
+	glfwMakeContextCurrent(window);
+
+	if (!gladLoadGLLoader((GLADloadproc) glfwGetProcAddress)) {
+		glfwMakeContextCurrent(nullptr);
+		glfwDestroyWindow(window);
+		glfwTerminate();
+		return -1;
+	}
+
+	glViewport(0, 0, 1440, 1080);
+	glfwSetFramebufferSizeCallback(window, [](GLFWwindow*, int width, int height) {
+		glViewport(0, 0, width, height);
+	});
+	glfwSetKeyCallback(window, [](GLFWwindow* window, int key, int scancode, int action, int mods) {
+		if (key == GLFW_KEY_ESCAPE && action == GLFW_PRESS)
+			glfwSetWindowShouldClose(window, true);
+	});
+
+	int success;
+	char infoLog[512];
+
+	char const* vertexShaderSource {
+		"#version 330 core\n"
+		"layout (location = 0) in vec3 aPos;\n"
+		"void main() {\n"
+		"	gl_Position = vec4(aPos.x, aPos.y, aPos.z, 1.0);\n"
+		"}\n"
+	};
+	unsigned int vertexShader {glCreateShader(GL_VERTEX_SHADER)};
+	glShaderSource(vertexShader, 1, &vertexShaderSource, nullptr);
+	glCompileShader(vertexShader);
+	glGetShaderiv(vertexShader, GL_COMPILE_STATUS, &success);
+	if (!success) {
+		glGetShaderInfoLog(vertexShader, sizeof(infoLog), nullptr, infoLog);
+		std::cerr << "ERROR::SHADER::VERTEX::COMPILATION_FAILED\n" << infoLog;
+		glDeleteShader(vertexShader);
+		glfwMakeContextCurrent(nullptr);
+		glfwDestroyWindow(window);
+		glfwTerminate();
+		return -1;
+	}
+
+	char const* fragmentShaderSource {
+		"#version 330 core\n"
+		"out vec4 FragColor;\n"
+		"void main() {\n"
+		"	FragColor = vec4(1.0f, 0.5f, 0.2f, 1.0f);\n"
+		"}\n"
+	};
+	unsigned int fragmentShader {glCreateShader(GL_FRAGMENT_SHADER)};
+	glShaderSource(fragmentShader, 1, &fragmentShaderSource, nullptr);
+	glCompileShader(fragmentShader);
+	glGetShaderiv(fragmentShader, GL_COMPILE_STATUS, &success);
+	if (!success) {
+		glGetShaderInfoLog(fragmentShader, sizeof(infoLog), nullptr, infoLog);
+		std::cerr << "ERROR::SHADER::FRAGMENT::COMPILATION_FAILED\n" << infoLog;
+		glDeleteShader(fragmentShader);
+		glDeleteShader(vertexShader);
+		glfwMakeContextCurrent(nullptr);
+		glfwDestroyWindow(window);
+		glfwTerminate();
+		return -1;
+	}
+
+	unsigned int shaderProgram {glCreateProgram()};
+	glAttachShader(shaderProgram, vertexShader);
+	glAttachShader(shaderProgram, fragmentShader);
+	glLinkProgram(shaderProgram);
+	glGetProgramiv(shaderProgram, GL_LINK_STATUS, &success);
+	if (!success) {
+		glGetProgramInfoLog(shaderProgram, sizeof(infoLog), nullptr, infoLog);
+		std::cerr << "ERROR::SHADER::PROGRAM::LINKING_FAILED\n" << infoLog;
+		glDeleteProgram(shaderProgram);
+		glDeleteShader(fragmentShader);
+		glDeleteShader(vertexShader);
+		glfwMakeContextCurrent(nullptr);
+		glfwDestroyWindow(window);
+		glfwTerminate();
+		return -1;
+	}
+
+	glDeleteShader(vertexShader);
+	glDeleteShader(fragmentShader);
+
+	float vertices[] {
+		 0.5f,  0.5f,  0.0f,
+		 0.5f, -0.5f,  0.0f,
+		-0.5f, -0.5f,  0.0f,
+		-0.5f,  0.5f,  0.0f
+	};
+
+	unsigned int indices[] {
+		0, 1, 3,
+		1, 2, 3
+	};
+
+	unsigned int VBO;
+	glGenBuffers(1, &VBO);
+	glBindBuffer(GL_ARRAY_BUFFER, VBO);
+	glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
+	glBindBuffer(GL_ARRAY_BUFFER, 0);
+
+	unsigned int EBO;
+	glGenBuffers(1, &EBO);
+	glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, EBO);
+	glBufferData(GL_ELEMENT_ARRAY_BUFFER, sizeof(indices), indices, GL_STATIC_DRAW);
+	glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, 0);
+
+	unsigned int VAO;
+	glGenVertexArrays(1, &VAO);
+	glBindVertexArray(VAO);
+	glBindBuffer(GL_ARRAY_BUFFER, VBO);
+	glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 3*sizeof(float), static_cast<void*>(0));
+	glEnableVertexAttribArray(0);
+	glBindBuffer(GL_ARRAY_BUFFER, 0);
+	glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, EBO);
+	glBindVertexArray(0);
+	glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, 0);
+
+	// glPolygonMode(GL_FRONT_AND_BACK, GL_LINE);
+	glClearColor(0.2f, 0.3f, 0.3f, 1.0f);
+
+	while(!glfwWindowShouldClose(window)) {
+		glClear(GL_COLOR_BUFFER_BIT);
+
+		glUseProgram(shaderProgram);
+		glBindVertexArray(VAO);
+		glDrawElements(GL_TRIANGLES, 6, GL_UNSIGNED_INT, 0);
+
+		glfwSwapBuffers(window);
+		glfwPollEvents();
+	}
+
+	glDeleteVertexArrays(1, &VAO);
+	glDeleteBuffers(1, &EBO);
+	glDeleteBuffers(1, &VAO);
+	glDeleteProgram(shaderProgram);
+	glfwMakeContextCurrent(nullptr);
+	glfwDestroyWindow(window);
+	glfwTerminate();
+	return 0;
+}
--- main.cc
+++ main.cc
@@ -1,12 +1,14 @@
 #include <iostream>
 
 int hello_window();
+int hello_triangle();
 
 int main() {
 	std::ios_base::sync_with_stdio(false);
 	std::cin.tie(nullptr);
 
-	auto const err = hello_window();
+	// auto const err = hello_window();
+	auto const err = hello_triangle();
 
 	std::cout << "Hello, world!\n";
 	return err;
